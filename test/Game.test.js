26ee5bd8a9d51750a6e60d8c89a5f95c3e26d310
Mathieu Barat <mathieu.barat@renovationman.com>
2022-02-07 11:44:48 +0100

    test that method exist

--- test/Game.test.js
+++ test/Game.test.js
@@ -4,6 +4,12 @@ import { Game } from "../src/Game";
 beforeAll(() => {});
 afterAll(() => {});
 
-it("Should ", () => {
+it("Should have a method named roll", () => {
   const game = new Game();
+  expect(game.roll).toBeDefined();
+});
+
+it("Should have a method named score", () => {
+  const game = new Game();
+  expect(game.score).toBeDefined();
 });
