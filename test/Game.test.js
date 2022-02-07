9b4f387fbc0174d4ccb26adc33a25d4f7cc0dbc0
Mathieu Barat <mathieu.barat@renovationman.com>
2022-02-07 11:16:12 +0100

    skeleton start

---
+++ test/Game.test.js
@@ -0,0 +1,10 @@
+// use named imports
+import { Game } from "../src/Game";
+
+beforeAll(() => {});
+afterAll(() => {});
+
+it("Should ", () => {
+  const game = new Game();
+  expect(game).toBe(false);
+});
